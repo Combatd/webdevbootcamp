908750110fa2ea7341201223dc28d123ba0487eb
combatd <combatdstar@gmail.com>
2019-09-06 00:14:36 -0700

    echo function

---
+++ introtonode/echo.js
@@ -0,0 +1,10 @@
+function echo(str, num) {
+    let i = 0;
+    while (i < num) {
+        console.log(str);
+        i++ // iterate by 1
+    }
+}
+
+echo("Echo!!!", 10); // test
+echo("Tater Tots", 3); // test
