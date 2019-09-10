33f09be735f1c6d39e5d7c563c2d7fefe1a170f3
combatd <combatdstar@gmail.com>
2019-09-10 05:50:08 -0700

    simple friends list

---
+++ moreexpress/postrequestdemo/app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+// POST route to add new friends
+app.post("/addfriend", function(req, res) {
+    var newFriend = req.body.newfriend; // all data from req body
+    friends.push(newFriend);
+    res.redirect("/friends"); // redirects back to friends
+});
+
+// "/friends"
+app.get("/friends", function(req, res) {
+    res.render("friends", {friends: friends});
+});
+
+app.listen(3000, function(){
+    console.log("Server listening on port 3000");
+});
--- moreexpress/readme.md
+++ moreexpress/readme.md
@@ -15,3 +15,9 @@ Loop through all posts
     title
     tagline
     ...
+
+# Post Requests!!!
+
+* Write post routes, and test them with Postman
+* Use a form to send a post request
+* Use body parser to get form data
