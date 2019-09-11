52ac4183eea51aaf3b9d01a8a9cbe652001c9d51
combatd <combatdstar@gmail.com>
2019-09-11 11:27:12 -0700

    movie_search_app complete

--- apis/movie_search_app/app.js
+++ apis/movie_search_app/app.js
@@ -4,9 +4,15 @@ const request = require('request');
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+    res.render("search");
+});
+
 // results route
 app.get("/results", function(req, res) {
-    request("http://omdbapi.com/?s=iowa&apikey=thewdb", (error, response, body) => {
+    var query = req.query.search;
+    var url = "http://omdbapi.com/?s=" + query + "&apikey=thewdb";
+    request(url, (error, response, body) => {
         if(!error && response.statusCode == 200) {
             var data = JSON.parse(body)
             res.render("results", {data: data} );
