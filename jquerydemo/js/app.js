e1b8b57a09649437158af5b696dde5def126948b
combatd <combatdstar@gmail.com>
2019-09-22 21:21:52 -0700

    selectorsexercise first commit

--- jquerydemo/js/app.js
+++ jquerydemo/js/app.js
@@ -1,4 +1,29 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+// const express = require("express");
+// const app = express();
+// const bodyParser = require("body-parser");
 // const $ = require("jquery"); // jquery
+
+const styles = {
+    color: "red",
+    background: "pink",
+    border: "2px solid purple"
+};
+
+$("h1").css(styles);
+$("li").css("color", "blue");
+
+let lis = document.querySelectorAll("li");
+
+for (let i = 0; i < lis.length; i++) {
+    lis[i].style.color = "green";
+}
+
+$("li").css("color", "blue");
+
+$("a").css("font-size", "40px");
+
+$("li").css({
+    fontSize: "10px",
+    border: "3px dashed purple",
+    background: "rgba(89, 45, 20, 0.5)"
+});
---
+++ jquerydemo/selectorsexercise/js/app.js
@@ -0,0 +1,6 @@
+// 1. Correctly include jQuery
+// 2. Select all divs and give a purple background
+// 3. Select the divs with class "highlight" and make them 200px wide
+// 4. Select the div with id "third" and give it an orange border
+// Bonus: Select the first div only and change its font color to pink
+
