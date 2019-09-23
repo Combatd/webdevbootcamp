8cf287e36ea28c8220354bd1c33e188c7af6c164
combatd <combatdstar@gmail.com>
2019-09-22 19:01:42 -0700

    script tag jquery

--- jquerydemo/js/app.js
+++ jquerydemo/js/app.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const $ = require("jquery"); // jquery
+// const $ = require("jquery"); // jquery
