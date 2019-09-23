0fa20a3701f491b148b659c9ef7a67974274a19e
combatd <combatdstar@gmail.com>
2019-09-23 16:04:35 -0700

    testing out jquery

---
+++ jquerydemo/text-and-html/js/app.js
@@ -0,0 +1,43 @@
+// $("h1").text(); // => jQuery Methods Demo Page
+
+// $("ul").text(); // => Skittles Starburst Twix
+
+// $('li').text(); // "SkittlesStarburstTwix";
+
+// $("h1").text("New Text!"); // => <h1>New Text!!!</h1>
+
+// $("h1").text(); // => "New Text!!!"
+
+// $('li').text("Rusty, Colt's dog, is adorable");
+// <li>Rusty, Colt's dog, is adorable</li> <li>Rusty, Colt's dog, is adorable</li> <li>Rusty, Colt's dog, is adorable</li>
+
+console.log($('ul').html()); // html() refers to innerHTML
+
+$('ul').html("<li>I hacked your UL!</li> <li>Rusty is still adorable</li>");
+// updates to 2 lis in ul
+
+$('li').html("<a href='Google.com'>CLICK ME TO GO TO GOOGLE!</a>");
+// changes 2 lis to google links
+
+// we will change the img to this image http://1.bp.blogspot.com/-CNlpt4uOiHI/Tqqp8hje0II/AAAAAAAAArk/ApSYg40A0jQ/s400/common-vampire-bat_505_600x450.jpg
+
+$('img').css("width", "200px"); // resize flying red fox to 200px
+// $('img').attr("src", "http://1.bp.blogspot.com/-CNlpt4uOiHI/Tqqp8hje0II/AAAAAAAAArk/ApSYg40A0jQ/s400/common-vampire-bat_505_600x450.jpg");
+// changed flying red fox to vampire bat
+
+// $('input').attr("type", "color"); changes to black
+
+$("img:first-of-type").attr("src", "http://1.bp.blogspot.com/-CNlpt4uOiHI/Tqqp8hje0II/AAAAAAAAArk/ApSYg40A0jQ/s400/common-vampire-bat_505_600x450.jpg"); 
+// first img only will change to vampire bat from flying red fox
+
+$("img").last().attr("src", "http://1.bp.blogspot.com/-CNlpt4uOiHI/Tqqp8hje0II/AAAAAAAAArk/ApSYg40A0jQ/s400/common-vampire-bat_505_600x450.jpg");
+// last img will change to vampire bat
+
+// .val() helps us extract the value from out of an element selected
+$("input").val(); // => "" // whatever is in the input
+
+$("input").val("Rusty Steele");
+// <input type"text" placeholder="your name"> => Rusty Steele
+$("input").val(""); // reset the input to empty
+
+console.log(%("select").val()); // => "Toad"
